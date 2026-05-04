7f34e814197f8d81d76b82a75b78848333234437
Chernov-V <ikomaz97@gmail.com>
2026-05-04 21:30:00 +0400

    Блок 3: Похожие фильмы 2

--- src/entities/movie/api/index.ts
+++ src/entities/movie/api/index.ts
@@ -2,4 +2,15 @@
  * Экспорт API для работы с фильмами
  */
 
-export { movieApi, useGetPopularMoviesQuery, useGetTopRatedMoviesQuery, useGetNowPlayingMoviesQuery, useGetUpcomingMoviesQuery, useSearchMoviesQuery, useGetMovieDetailsQuery, useDiscoverMoviesQuery, useGetGenresQuery } from './movieApi'
+export {
+  movieApi,
+  useGetPopularMoviesQuery,
+  useGetTopRatedMoviesQuery,
+  useGetNowPlayingMoviesQuery,
+  useGetUpcomingMoviesQuery,
+  useSearchMoviesQuery,
+  useGetMovieDetailsQuery,
+  useDiscoverMoviesQuery,
+  useGetGenresQuery,
+  useGetSimilarMoviesQuery,
+} from './movieApi'
